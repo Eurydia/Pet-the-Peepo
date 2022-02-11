7e626bde3cbdda4ab5b04876b51c250674e2170f
Eurydia <84164836+Eurydia@users.noreply.github.com>
2022-02-11 09:11:02 +0700

    pepeSad

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,11 @@ const PEPE_IMG = document.querySelector("#🐸");
 const PEPE_AUDIO = document.querySelector("#🎵");
 
 document.addEventListener("mousedown", () => {
-    PEPE_IMG.src = "./src/img/PET_THE_PEEPO.webp";
+    PEPE_IMG.src = "./src/media/PET-THE-PEEPO.webp";
     PEPE_AUDIO.play();
 });
 
 document.addEventListener("mouseup", () => {
-    PEPE_IMG.src = "./src/img/pepeSad-static.png";
+    PEPE_IMG.src = "./src/media/pepe-static.png";
     PEPE_AUDIO.pause();
 });
